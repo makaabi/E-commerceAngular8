54e4ed82172d7921f0ec8b80223f1e5e54885dcc
makaabi <maminekaabi@gmail.com>
2019-11-24 20:38:43 +0100

    version 1

--- src/app/application/rechercherarticle/rechercherarticle.component.ts
+++ src/app/application/rechercherarticle/rechercherarticle.component.ts
@@ -1,21 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../../article';
+import {DataService} from '../../data.service';
+
 
 @Component({
   selector: 'app-rechercherarticle',
   template: `
   <app-menu></app-menu>
-
-    <p>
-      rechercherarticle works!
-    </p>
+  <h2 class="text-center text-danger">Liste Articles</h2>
+  <h3 class="text-success">Search by Price</h3>
+  <div class="active-pink-3 active-pink-4 mb-4">
+  <input class="form-control" type="text" placeholder="search by price" [(ngModel)]="prix" aria-label="Search">
+  </div>
+  <h3 class="text-success">Search by name</h3>
+  <div class="active-pink-3 active-pink-4 mb-4">
+  <input class="form-control" type="text" placeholder="search by name" [(ngModel)]="name" aria-label="Search">
+  </div>
+  <div class="container">
+  <ul class="list-group" >
+  <ng-container *ngFor="let t of lesarticles" >
+  <li class="list-group-item" class="bgl" *ngIf="t.prix>=prix && t.libelle.toUpperCase().indexOf(name.toUpperCase())!=-1" >
+  <div class="mgl">
+  <img [src]="t.photo" >
+  <ul>
+  <li > <span class="text-info">Matricule:</span> {{t.matricule}}</li>
+  <li> <span class="text-info">Libelle:</span>{{t.libelle}}</li>
+  <li> <span class="text-info">Prix:</span>{{t.prix}}dt</li>
+  </ul>
+  </div>
+  <hr>
+  </li>
+  </ng-container>
+  </ul>
+  </div>
   `,
-  styles: []
+  styles: [
+    '.bgl {background-color:#ffe6e6	;}li{list-style-type:none}.mgl{margin-left:100px;}hr{backgound-color:blue;}'
+
+  ]
 })
 export class RechercherarticleComponent implements OnInit {
+  lesarticles:Article[];
+  prix:number=0;
+  name:string="";
 
-  constructor() { }
+  constructor(private dataService:DataService) { }
 
   ngOnInit() {
+    this.lesarticles = this.dataService.lesArticles;
+
   }
 
 }
--- src/app/application/unarticle/unarticle.component.ts
+++ src/app/application/unarticle/unarticle.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../../article';
+import {DataService} from '../../data.service';
+
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-unarticle',
   template: `
-    <p>
-      unarticle works!
-    </p>
+  <h2 class="text-center text-info display-2"> Article {{obj.matricule}}</h2>
+  <ul class="list-group">
+  <li class="list-group-item " ><img [src]="obj.photo" id="imgl"></li>
+  <li class="list-group-item" >matricule:<span class="text-danger font-weight-bold">{{obj.matricule}}</span></li>
+  <li class="list-group-item" >libelle:<span class="text-danger font-weight-bold">{{obj.libelle}}</span></li>
+  <li class="list-group-item" >marque:<span class="text-danger font-weight-bold">{{obj.marque|uppercase}}</span></li>
+  <li class="list-group-item" >prix:<span class="text-danger font-weight-bold">{{obj.prix}}</span></li>
+  <li class="list-group-item" >date Fabrication:<span class="text-danger font-weight-bold">{{obj.dateFabrication|date: 'yyyy'}}</span></li>
+  <li class="list-group-item"[ngClass]="(obj.enstock)?'c1':'c2'">en Stock</li>
+  <li class="list-group-item" ><span class="text-success font-weight-bold">{{obj.prix|garant}}</span></li>
+  </ul>
+    <button class="btn btn-dark ml-2" (click)="onArticles()">Retour</button>
   `,
-  styles: []
+  styles: [
+    '#imgl{margin-left:30%;}.c1{background-color:green}.c2{background-color:red}'
+
+  ]
 })
 export class UnarticleComponent implements OnInit {
+  obj:Article;
+  matricule:number;
 
-  constructor() { }
+  constructor(private activatedRoute:ActivatedRoute,private dataService:DataService,private router:Router) { }
 
   ngOnInit() {
+    this.matricule = this.activatedRoute.snapshot.params['matricule'];
+    this.obj=this.dataService.getEmployeByMatriculeServ(this.matricule);
   }
 
 }
